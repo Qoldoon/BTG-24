080e6171dbcab635ae32b656ea04b94b2b057a13
Emilis <46862485+Qoldoon@users.noreply.github.com>
2025-08-28 13:47:03 +0300

    tilemaps

--- Game/Assets/Scripts/PlayerScripts/PlayerInventory.cs
+++ Game/Assets/Scripts/PlayerScripts/PlayerInventory.cs
@@ -85,6 +85,7 @@ public class PlayerInventory : MonoBehaviour
     public void Toss(Vector2 direction, Item item)
     {
         if(item == null) return;
+        item.UnEquip();
         var pickup = Instantiate(pick, transform.position, Quaternion.identity);
         pickup.GetComponent<Pickup>().item = item.gameObject;
         item.transform.SetParent(pickup.transform);
--- Game/Assets/Scripts/WallState.cs
+++ Game/Assets/Scripts/WallState.cs
@@ -1,26 +1,29 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class WallState : MonoBehaviour,  IDamageable
 {
-    public Sprite damagedsprite;
-    SpriteRenderer spriteRenderer;
-    private BigWall parent;
+    private Tilemap tilemap;
+    private Vector3Int cellPosition;
     
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        parent = GetComponentInParent<BigWall>();
+        tilemap = GetComponentInParent<Tilemap>();
+        cellPosition = tilemap.WorldToCell(transform.position);
     }
 
     public HitResponse Hit(float damage, int target, bool emp = false)
     {
         HitResponseBuilder hb = new HitResponseBuilder().ForAll();
         if(damage <= 120f) return hb.Destroy().Build();
-        spriteRenderer.sprite = damagedsprite;
-        GetComponent<Collider2D>().enabled = false;
-        if(parent != null)
-            parent.Scan();
-        else AstarPath.active?.Scan();
+        DestroyWall();
         return hb.Build();
+    }
+
+    private void DestroyWall()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        AstarPath.active?.Scan();
+        tilemap.SetTile(cellPosition, null);
     }
 }
